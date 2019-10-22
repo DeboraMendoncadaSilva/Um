96b681fe735914012dc10caa4565c686dbe9d61d
debora-mds <55601339+debora-mds@users.noreply.github.com>
2019-10-21 21:51:57 -0400

    Add files via upload

--- AssociacaoAluno.php
+++ AssociacaoAluno.php
@@ -6,16 +6,16 @@ include_once "Turma.class.php";
 
 //Instanciamentos da classe Aluno
 
-$aluno = new Aluno;
-$aluno->nome= "Debora Mendonca da Silva";
-$aluno->idade= 29;
-$aluno->turma= "Informatica";
+$aluno1 = new Aluno1;
+$aluno1->nome= "Debora Mendonca da Silva";
+$aluno1->idade= 29;
+$aluno1->turma= "Informatica";
 
 
-$aluno = new Aluno;
-$aluno->nome= "Thiago Mendonca da Silva";
-$aluno->idade= 25;
-$aluno->turma= "Informatica";
+$aluno2 = new Aluno2;
+$aluno2->nome= "Thiago Mendonca da Silva";
+$aluno2->idade= 25;
+$aluno2->turma= "Informatica";
 
 //Instaciamento da Turma
 
@@ -29,11 +29,11 @@ $turma->ocupacao = "04";
 //Exibindo os atributos na tela
 
 echo "<h4> Imprimindo os Dados</h4>";
-echo "Nome do Aluno: ".$aluno->nome. "<br>";
-echo "Idade do Aluno: ".$aluno->idade. "<br>";
-echo "Turma do Aluno: ".$aluno->turma->codigo. "<br>";
-echo "Turma do Aluno: ".$aluno->turma->descricao. "<br>";
-echo "Turma do Aluno: ".$aluno->turma->ocupacao. "<br>";
+echo "Nome do Aluno1: ".$aluno1->nome. "<br>";
+echo "Idade do Aluno1: ".$aluno1->idade. "<br>";
+echo "Turma do Aluno1: ".$aluno1->turma->codigo. "<br>";
+echo "Turma do Aluno1: ".$aluno1->turma->descricao. "<br>";
+echo "Turma do Aluno1: ".$aluno1->turma->ocupacao. "<br>";
 
 echo "<h4> Imprimindo os Dados</h4>";
 echo "Nome do Aluno2: ".$aluno2->nome. "<br>";
